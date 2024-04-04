c18eeee75b8ea6e787a95c8f1fa006d307e568bf
Fembunny <bellacyrne@gmail.com>
2024-04-04 02:36:57 -0300

    ProdutoRepository
    Criação do package repository e da classe ProdutoRepository

---
+++ EsquinaBeer/src/main/java/com/senac/esquinaBeer/repository/ProdutoRepository.java
@@ -0,0 +1,19 @@
+package com.senac.esquinaBeer.repository;
+
+import com.senac.esquinaBeer.model.ProdutoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<ProdutoEntity, Integer> {
+    Optional<ProdutoEntity> findById(Integer id); // Consulta por ID
+
+    List<ProdutoEntity> findByNomeContainingIgnoreCase(String nome); // Consulta por nome (ignorando maiúsculas/minúsculas) usando JPQL
+
+    List<ProdutoEntity> findByCategoriaContainingIgnoreCase(String categoria); // Consulta por categoria (ignorando maiúsculas/minúsculas) usando JPQL
+
+    List<ProdutoEntity> findByPreco(Double preco); // Consulta por preço usando JPQL
+}
+
